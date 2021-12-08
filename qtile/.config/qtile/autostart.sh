24c615348f4cb461a3bc84d33c7475476c7e8e56
Sanjay Pavan <tsppavan7@gmail.com>
2021-12-08 17:23:38 +0530

    Some changes

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -4,6 +4,7 @@ killall -9 picom dunst sxhkd nm-applet blueman-applet volumeicon flameshot
 
 picom &
 xrandr --dpi 96 &
+xsetroot -cursor_name left_ptr &
 dunst &
 lxsession &
 xwallpaper --zoom $HOME/.config/qtile/nord.png &
