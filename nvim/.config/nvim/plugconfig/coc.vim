ab7a3944b1b71f7cc102e63f528b036f0e7915d3
Sanjay Pavan <tsppavan7@gmail.com>
2021-12-30 16:00:05 +0530

    Some more changes

--- nvim/.config/nvim/plugconfig/coc.vim
+++ nvim/.config/nvim/plugconfig/coc.vim
@@ -5,6 +5,7 @@ let g:coc_global_extensions = [
 	\ 'coc-tsserver',
 	\ 'coc-html',
 	\ 'coc-css',
+	\ 'coc-sh',
 	\ 'coc-json',
 	\ 'coc-python',
 	\ 'coc-marketplace'
--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -7,12 +7,11 @@ xrandr --dpi 96 &
 xsetroot -cursor_name left_ptr &
 dunst &
 lxsession &
-xwallpaper --zoom $HOME/.config/qtile/nord.png &
 sxhkd &
+xrdb -load ~/.config/x11/xresources &
 setbg &
 nm-applet &
 blueman-applet &
 volumeicon &
 flameshot &
 brightnessctl s 5 &
-thunar --daemon &
