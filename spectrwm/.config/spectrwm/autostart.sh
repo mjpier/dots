38900b435a32ef249a118c66051cb59ab806c592
Sanjay Pavan <withercubes@protonmail.com>
2022-02-17 19:55:37 +0530

    Update to spectrwm

--- spectrwm/.config/spectrwm/autostart.sh
+++ spectrwm/.config/spectrwm/autostart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+killall -9 picom dunst sxhkd nm-applet blueman-applet flameshot
+
 picom &
 xrandr --dpi 96 &
 xsetroot -cursor_name left_ptr &
@@ -10,6 +12,6 @@ xrdb -load ~/.config/x11/xresources &
 setbg ~/.local/share/wall/evening-sky.png &
 nm-applet &
 blueman-applet &
-volumeicon &
 flameshot &
 brightnessctl s 5 &
+thunar --daemon &
